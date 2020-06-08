e7ea5556abe7241e9f97e1e04f4c320e164548f8
jorgen <jorgentreidersmerud@gmail.com>
2020-06-08 19:21:05 +0200

    Clear out dir

--- out/utils.d.ts
+++
@@ -1,8 +0,0 @@
-import * as protocol from './messages';
-export declare const isBrowser: boolean;
-export declare const isNode: boolean;
-export declare function boolean(value: any): value is boolean;
-export declare function string(value: any): value is string;
-export declare function number(value: any): value is number;
-export declare function ndefined(value: any): value is undefined;
-export declare function notification(message: protocol.Message | undefined | any): message is protocol.NotificationMessage<any>;
